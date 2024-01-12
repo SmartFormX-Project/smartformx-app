b423d3eac4dff0848ac9bb4f98fd96db883b0fdb
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-01-12 16:14:45 -0300

    solved bug fecth plan

--- app/(backend)/api/payment/plans/route.ts
+++ app/(backend)/api/payment/plans/route.ts
@@ -2,7 +2,20 @@
 import { NextRequest, NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest, res: any) {
+
+   // Set CORS headers
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+ 
+   // Handle preflight requests
+   if (req.method === 'OPTIONS') {
+     res.status(200).end();
+     return;
+   }
+
+   
   const products = await stripe.products.list({
     limit: 3,
     active: true,
