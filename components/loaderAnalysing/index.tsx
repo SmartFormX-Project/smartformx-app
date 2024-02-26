af8235c804119797a8862830c9488fdd2c813cf6
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-02-25 21:37:13 -0300

    update front loading analyse

--- components/loaderAnalysing/index.tsx
+++ components/loaderAnalysing/index.tsx
@@ -5,6 +5,7 @@ function LoaderComponent() {
   return (
     <div className="wrapper">
       <span>Analisando...</span>
+      <p>Isso pode levar um tempo</p>
       <div className="loader"></div>
     </div>
   );
